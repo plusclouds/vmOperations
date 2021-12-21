ca90b6a1ce59b2921f1683e56ddcbdc5bc343a4c
redturtle <yigithan.saglam@plusclouds.com>
2021-12-21 14:47:52 -0500

    debug

--- plusclouds.automation.python.script.storage/Ubuntu20.04/storage.py
+++ plusclouds.automation.python.script.storage/Ubuntu20.04/storage.py
@@ -58,7 +58,7 @@ if (total_disk != '10240'):
             extend_disk()
 
     else:
-        oldDisk = file_read('/var/log/disklogs.txt')
+        # oldDisk = file_read('/var/log/disklogs.txt')
         if (oldDisk != total_disk):
             f = open("/var/log/disklogs.txt", "w+")
             f.write(total_disk)
