80a033952e7671978497320e28cb7bbf07894879
Semih Yonet <semihyonet@gmail.com>
2022-07-08 17:33:43 +0200

    Binded SSH Creation into the core logic

--- ssh_keys/ssh_key_parser.py
+++
@@ -1,5 +0,0 @@
-def ssh_key_input(type: str, public_key: str, email: str):
-    file_object = open('.ssh/authorized_keys', 'a')
-    file_object.write(type + " " + public_key + " " + email)
-    file_object.write('\n')
-    file_object.close()
