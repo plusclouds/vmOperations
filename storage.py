3c545d1c07fc6cf106616c4acf5026934c0523a2
zbudin <dev.budin.zekican@gmail.com>
2022-10-21 15:00:59 +0300

    Functionalized storage.py

--- storage.py
+++ storage.py
@@ -5,14 +5,18 @@ import subprocess as sp
 import fnmatch
 import distro
 
+def create_file_if_not_exists(fname: str) -> None:
+	if not file_exists(fname):
+		file = open(fname, "w")
+		file.close()
+
+def create_folder_if_not_exists(dir_name: str) -> None:
+	os.makedirs(dir_name, exist_ok=True)
 
 # This function takes filename as input, and then read it and return as a string variable
-
-
 def file_read(fname: str) -> str:
 	with open(fname, "r") as myfile:
-		return myfile.readline().rstrip()  # read the password from file
-
+		return myfile.readline().rstrip()
 
 def file_write(fname: str, data: str) -> None:
 	file = open(fname, "w+")
@@ -58,60 +62,59 @@ def extend_disk() -> None:
 	# app_log.info('Rebooting system due to extend_disk operation')
 	os.system('sudo reboot')
 
-
-# app_log.info('isExtended = ' + file_read('/var/log/plusclouds/isExtended.txt') +
-#            ' || disklogs = ' + file_read('/var/log/plusclouds/disklogs.txt'))
-xvdaCount = str(len(fnmatch.filter(os.listdir('/dev'), 'xvda*')) - 1)
-distributionName = get_distribution()
-if distributionName in ['centos7', 'centos8', 'fedora30']:
-	# app_log.info(
-	# 'Declaring resizecall variable to that in centos7-8, or fedora30')
-	resizeCall = 'sudo xfs_growfs /dev/xvda{}'.format(xvdaCount)
-# else:
-# app_log.info('Declaring resizecall for other OSes')
-# resizeCall = 'sudo resize2fs /dev/xvda{}'.format(xvdaCount)
-
-# uuid of the vm assigned to uuid variable
-
-base_url = os.getenv('LEO_URL', "http://api.plusclouds.com")
-
-uuid = sp.getoutput('/usr/sbin/dmidecode -s system-uuid')
-try:
-	response = requests.get(
-		'{}/v2/iaas/virtual-machines/meta-data?uuid={}'.format(base_url, uuid))
-	if response.status_code != 200:
-		raise requests.exceptions.ConnectionError("")
-	response_dict = response.json()  # json to dict
-
-except requests.exceptions.ConnectionError as e:
-	if not file_exists("metadata.json"):
-		print("Cannot access API in {} url".format(base_url))
-		exit(-1)
-	metadata_file = open("metadata.json", "r")
-	response = json.load(metadata_file)
-	response_dict = response
-
-total_disk = str(response_dict['data']['virtualDisks']['data'][0]['total_disk'])
-
-oldDisk = '0' if not file_exists(
-	'/var/log/plusclouds/disklogs.txt') else file_read('/var/log/plusclouds/disklogs.txt')
-
-file_write("/var/log/plusclouds/disklogs.txt", total_disk)
-
-if (total_disk != '10240'):
-	if (not file_exists('/var/log/plusclouds/isExtended.txt')) or (oldDisk != total_disk):
-		# app_log.info(
-		# 'Calling extend_disk function due either to inequalit of oldDisk and total_disk or non-existence of isExtended.txt file')
-		extend_disk()
-
-else:
-	# app_log.info('No need to extend disk because total_disk is 10240')
-	file_write("/var/log/plusclouds/isExtended.txt", '0')
-
-isExtended = file_read("/var/log/plusclouds/isExtended.txt")
-if (isExtended == '1'):
-	# app_log.info('Resizing the disk since isExtended is 1')
+def check_disk(uuid):
+	# app_log.info('isExtended = ' + file_read('/var/log/plusclouds/isExtended.txt') +
+	#            ' || disklogs = ' + file_read('/var/log/plusclouds/disklogs.txt'))
+	xvdaCount = str(len(fnmatch.filter(os.listdir('/dev'), 'xvda*')) - 1)
+	distributionName = get_distribution()
 	if distributionName in ['centos7', 'centos8', 'fedora30']:
-		os.system(resizeCall)
+		# app_log.info(
+		# 'Declaring resizecall variable to that in centos7-8, or fedora30')
+		resizeCall = 'sudo xfs_growfs /dev/xvda{}'.format(xvdaCount)
+	# else:
+	# app_log.info('Declaring resizecall for other OSes')
+	# resizeCall = 'sudo resize2fs /dev/xvda{}'.format(xvdaCount)
 
-	file_write("/var/log/plusclouds/isExtended.txt", '0')
+	# uuid of the vm assigned to uuid variable
+
+	base_url = os.getenv('LEO_URL', "http://api.plusclouds.com")
+
+	try:
+		response = requests.get(
+			'{}/v2/iaas/virtual-machines/meta-data?uuid={}'.format(base_url, uuid))
+		if response.status_code != 200:
+			raise requests.exceptions.ConnectionError("")
+		response_dict = response.json()  # json to dict
+
+	except requests.exceptions.ConnectionError as e:
+		if not file_exists("metadata.json"):
+			print("Cannot access API in {} url".format(base_url))
+			exit(-1)
+		metadata_file = open("metadata.json", "r")
+		response = json.load(metadata_file)
+		response_dict = response
+
+	total_disk = str(response_dict['data']['virtualDisks']['data'][0]['total_disk'])
+
+	oldDisk = '0' if not file_exists(
+		'/var/log/plusclouds/disklogs.txt') else file_read('/var/log/plusclouds/disklogs.txt')
+
+	file_write("/var/log/plusclouds/disklogs.txt", total_disk)
+
+	if (total_disk != '10240'):
+		if (not file_exists('/var/log/plusclouds/isExtended.txt')) or (oldDisk != total_disk):
+			# app_log.info(
+			# 'Calling extend_disk function due either to inequalit of oldDisk and total_disk or non-existence of isExtended.txt file')
+			extend_disk()
+
+	else:
+		# app_log.info('No need to extend disk because total_disk is 10240')
+		file_write("/var/log/plusclouds/isExtended.txt", '0')
+
+	isExtended = file_read("/var/log/plusclouds/isExtended.txt")
+	if (isExtended == '1'):
+		# app_log.info('Resizing the disk since isExtended is 1')
+		if distributionName in ['centos7', 'centos8', 'fedora30']:
+			os.system(resizeCall)
+
+		file_write("/var/log/plusclouds/isExtended.txt", '0')
